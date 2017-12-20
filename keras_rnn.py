f3ca027d5d477832c6926f4b0d02e1d1a194f0a6
Kegan Rabil <trisectionage@gmail.com>
2017-12-19 22:19:27 -0700

    updated RNN

--- keras_rnn.py
+++ keras_rnn.py
@@ -12,6 +12,7 @@ import dialog_parser
 
 from keras.models import Model
 from keras.layers import Input, LSTM, Dense
+from keras.preprocessing import sequence
 import numpy as np
 # fix random seed for reproducibility
 np.random.seed(36)
@@ -90,25 +91,41 @@ num_samples = 10000  # Number of samples to train on.
 # max length of 100 excludes about 1200 of 300k samples.
 # max length of 200 excludes about 100 of 300k
 max_seq_len = 100
+embedding_vecor_length = 1028#32
+top_words = len(vocab)
+split_index = int(len(input_seq) * 0.8)
 
 
 # load the dataset but only keep the top n words, zero the rest
-top_words = 5000
-(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
+X_train = input_seq[:split_index]
+X_test = input_seq[split_index:]
+y_train = target_seq[:split_index]
+y_test = target_seq[split_index:]
 # truncate and pad input sequences
 X_train = sequence.pad_sequences(X_train, maxlen=max_seq_len)
 X_test = sequence.pad_sequences(X_test, maxlen=max_seq_len)
 
-HODOR
-# create the model
-embedding_vecor_length = 32
-model = Sequential()
-model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
-model.add(LSTM(100))
-model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-print(model.summary())
-model.fit(X_train, y_train, nb_epoch=3, batch_size=64)
-# Final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("Accuracy: %.2f%%" % (scores[1]*100))
+
+
+######################## MODEL #############################
+# Define an input sequence and process it.
+encoder_inputs = Input(shape=(None, num_encoder_tokens))
+encoder = LSTM(latent_dim, return_state=True)
+encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+# We discard `encoder_outputs` and only keep the states.
+encoder_states = [state_h, state_c]
+
+# Set up the decoder, using `encoder_states` as initial state.
+decoder_inputs = Input(shape=(None, num_decoder_tokens))
+# We set up our decoder to return full output sequences,
+# and to return internal states as well. We don't use the
+# return states in the training model, but we will use them in inference.
+decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
+                                     initial_state=encoder_states)
+decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs)
+
+# Define the model that will turn
+# `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
